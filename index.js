268b243f40ab1859c2f19bc30ba502517a64a6e3
bmlaghui <bmlaghui@gmail.com>
2022-04-27 18:20:38 +0200

    api update

--- index.js
+++ index.js
@@ -41,10 +41,68 @@ app.get('/questionsQCM/:matiere/:nbQuestions', (req,res, next) => {
             return next(err);
         }
 
-       
+
         res.setHeader("Content-Type", "application/json; charset=utf-8");
 
         res.end(JSON.stringify(docs))
+    })
+})
+
+
+
+//formatted_data
+app.get('/questionsQUIZ/:matiere/:nbQuestions', (req,res, next) => {
+    db.collection(req.params.matiere).aggregate([{"$project": {"_id": 0, "question": 1, "correctOption": 1, "optionA": 1, "optionB": 1,
+            "optionC": 1, "optionD": 1, "numero": 1}},
+        {"$sample": {"size": parseInt(req.params.nbQuestions)}}]).toArray(function(err, docs) {
+        if (err) {
+            console.log(err)
+            res.status(500);
+            return next(err);
+        }
+
+        var dictALL = [];
+        var i =-1
+        for(item of docs) {
+            var dict = {};
+            i++
+            var questionText = item.question;
+            var optiona = {"text": item.optionA}
+            var optionb = {"text": item.optionB}
+            var optionc = {"text": item.optionC}
+            var optiond = {"text": item.optionD}
+            if (item.correctOption == 1) {
+                var optiona = {"text": item.optionA, "correct": true}
+            }
+            else if (item.correctOption == 2) {
+                var optionb = {"text": item.optionB, "correct": true}
+            }
+            else if (item.correctOption == 3) {
+                var optionc = {"text": item.optionC, "correct": true}
+            }
+            else if (item.correctOption == 4) {
+                var optiond = {"text": item.optionD, "correct": true}
+            }
+
+
+            var options = [optiona, optionb, optionc, optiond]
+
+
+
+            dict["questionText"] = questionText
+            dict["options"] = options
+            dictALL[i] = dict
+
+        }
+
+        console.log(dictALL)
+        let dictRes = {"questions":dictALL}
+
+
+
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
+        //console.log(JSON.stringify(dictRes))
+        res.end(JSON.stringify(dictRes))
     })
 })
 
--- readme.md
+++ readme.md
@@ -1,2 +1,15 @@
 npm install express --save
 nodemon : lancer le erveur
+
+/*
+let obj = {};
+
+        console.log("*****")
+        for(item of docs) {
+            obj['questions'] = ['questionText', 'options'];
+            obj['questions']['questionText'] = item.question;
+            obj['questions']['options'] = item.optionA;
+        }
+        console.log(obj)
+
+*/
