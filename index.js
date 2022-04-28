b0ae98fec4daa752bd64d569b43b6cdf869436aa
bmlaghui <bmlaghui@gmail.com>
2022-04-28 04:52:06 +0200

    install depancies

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ app.get('/questionsQUIZ/:matiere/:nbQuestions', (req,res, next) => {
 })
 
 
-app.listen(8080, () => {
-    console.log("Serveur à l'écoute")
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Serveur à l'écoute au port 5000")
 })
