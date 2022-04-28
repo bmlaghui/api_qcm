ed5a1d0cc8d2a9446c35c58298184a80ce6d05aa
bmlaghui <bmlaghui@gmail.com>
2022-04-28 05:24:45 +0200

    updated

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ const mongoose = require('mongoose');
 /**
  * Import MongoClient & connexion à la DB
  */
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'QCMQUIZ';
-let db
-
-MongoClient.connect(url, function(err, client) {
-    console.log("Connected successfully to server");
-    db = client.db(dbName);
-});
 
 const username = "bmlaghui";
 const password = "bmlaghui";
