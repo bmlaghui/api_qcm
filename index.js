a97a3117f7468be74bd5a4fe992b4bb28b8208f1
bmlaghui <bmlaghui@gmail.com>
2022-04-28 05:00:32 +0200

    env file added

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ app.get('/questionsQUIZ/:matiere/:nbQuestions', (req,res, next) => {
 })
 
 
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log("Serveur à l'écoute au port 5000")
 })
