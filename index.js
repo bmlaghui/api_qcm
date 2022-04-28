44d82aaf5236b88233bd8cc4a6ca30de5fc80a00
bmlaghui <bmlaghui@gmail.com>
2022-04-28 06:20:00 +0200

    updated

--- index.js
+++ index.js
@@ -108,6 +108,6 @@ app.get('/questionsQUIZ/:matiere/:nbQuestions', (req,res, next) => {
 })
 
 
-app.listen(process.env.port || 3000, function(){
+app.listen(process.env.port || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
